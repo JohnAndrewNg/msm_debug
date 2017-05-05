dc93dedf1f01a79a96012ae31071994a92ea8bb6
John Ng <john.ng@kellogg.northwestern.edu>
2017-05-05 00:34:00 -0500

    Fixed movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,46 +1,59 @@
 class MoviesController < ApplicationController
+
+#READ
   def index
     @movies = Movie.all
+    render("movies/index.html.erb")
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params["id"])
+    render("movies/show.html.erb")
   end
 
+#CREATE
   def new_form
+    render("movies/new_form.html.erb")
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie = Movie.new
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
 
     @movie.save
 
-    render("show")
+    render("movies/show.html.erb")
   end
 
+#UPDATE
   def edit_form
     @movie = Movie.find(params[:id])
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie = Movie.find(params["id"])
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
+    @movie.save
 
-    render("show")
+    render("movies/show.html.erb")
   end
 
+#DELETE
   def destroy
-    movie = Movie.find(params[:id])
-
+    movie = Movie.find(params["id"])
+    @movietitle = movie.title
     movie.destroy
+
+    render("movies/destroy.html.erb")
   end
 end
